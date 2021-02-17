98a455268f244b6896c623de5c5dfe8f8809fb33
TheSims <Skelet_91@hotmail.com>
2021-02-17 10:07:49 +0100

    hejm ed jer

--- ECS.Legacy/TempSensor.cs
+++ ECS.Legacy/TempSensor.cs
@@ -10,9 +10,14 @@ namespace ECS.Legacy
         {
             return gen.Next(-5, 45);
         }
+<<<<<<< HEAD
         //hej med dig
+=======
+        //hello world
+>>>>>>> 743dff3f41be27b3f50cfa1b3027f9cc14540918
         public bool RunSelfTest()
             //hej med jer alle
+            //hbaas
         {
             return true;
         }
